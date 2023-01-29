e856e678274dadaead0ddea8fa63a99ad2870ef5
Nik <Nik20906@gmail.com>
2023-01-29 16:05:09 +0530

    add loop and sort the code simple and easy to learn

--- game.js
+++ game.js
@@ -8,6 +8,8 @@ let time_number = document.querySelector(".time_number")
 let  score_number= document.querySelector(".score_number")
 let  select_img= document.querySelector(".select_img")
 
+let pic = document.querySelectorAll(".select_img img")
+
 let  first_img= document.querySelector(".first_img")
 let  second_img= document.querySelector(".second_img")
 let  third_img= document.querySelector(".third_img")
@@ -21,21 +23,14 @@ btn.onclick=()=>{
     let count_scor=1;
     let a=viewportDiv.clientHeight
     let b=viewportDiv.clientWidth
-
-
-    first_img.onclick=()=>{
-
-    
-
-   
-
-   
-
+    for(let i=0;i<pic.length;i++){
+      
+    pic[i].onclick=()=>{
+       
+            console.log("asfajsjnxdncfbnasnndnsamndxmsZN")
         select_img.style.display="none"
         header.style.display="block"
-
-        
-        setInterval(()=>{
+            setInterval(()=>{
         
             time_number.innerHTML=counter++;
             },1000);
@@ -46,7 +41,17 @@ btn.onclick=()=>{
             let h=Math.random()*(a)
             let j=Math.random()*(b)
             let imz = document.createElement("img")
-    imz.setAttribute("src","https://freepngimg.com/thumb/bart_simpson/3-2-bart-simpson-picture-thumb.png")
+            if(i==0){
+                imz.setAttribute("src","https://freepngimg.com/thumb/bart_simpson/3-2-bart-simpson-picture-thumb.png")
+            }
+            if(i==1){
+                imz.setAttribute("src","https://freepngimg.com/thumb/anime/12-2-anime-download-png-thumb.png")
+            }
+            if(i==2){
+                imz.setAttribute("src","https://freepngimg.com/thumb/doraemon/35004-7-doraemon-transparent-image-thumb.png")
+            }
+            
+    
     imz.setAttribute("onclick","imgclick(this)")
     imz.classList.add("autocreatimg")
     viewportDiv.appendChild(imz)
@@ -63,78 +68,118 @@ btn.onclick=()=>{
     
 }
 
-        },1000);
+        },600);
+
+        }
+    }
+    
+
+//     first_img.onclick=()=>{
+   
+//         select_img.style.display="none"
+//         header.style.display="block"
+
+        
+//         setInterval(()=>{
+        
+//             time_number.innerHTML=counter++;
+//             },1000);
+            
+             
+//                 setInterval(()=>{
+//                     if(counter<=30){
+//             let h=Math.random()*(a)
+//             let j=Math.random()*(b)
+//             let imz = document.createElement("img")
+//     imz.setAttribute("src","https://freepngimg.com/thumb/bart_simpson/3-2-bart-simpson-picture-thumb.png")
+//     imz.setAttribute("onclick","imgclick(this)")
+//     imz.classList.add("autocreatimg")
+//     viewportDiv.appendChild(imz)
+//     imz.style.top=h+'px'
+//     imz.style.left=j+'px'
+
+//     imz.onclick=()=>{
+//       imz.remove()
+//       score_number.innerHTML = count_scor++;
+
+//     }
+// }else{
+//     alert("Time Out....\n"+"Your Score ..."+(count_scor-1))
+    
+// }
+
+//         },1000);
 
     
         
-    }
+//     }
 
-    second_img.onclick=()=>{
-        select_img.style.display="none"
-        header.style.display="block"
+//     second_img.onclick=()=>{
+//         select_img.style.display="none"
+//         header.style.display="block"
 
         
-        setInterval(()=>{
+//         setInterval(()=>{
         
-            time_number.innerHTML=counter++;
-            },1000);
-        setInterval(()=>{
-            if(counter<=30){
-            let h=Math.random()*(a)
-            let j=Math.random()*(b)
-            let imz = document.createElement("img")
-    imz.setAttribute("src","https://freepngimg.com/thumb/anime/12-2-anime-download-png-thumb.png")
-    imz.setAttribute("onclick","imgclick(this)")
-    imz.classList.add("autocreatimg")
-    viewportDiv.appendChild(imz)
-    imz.style.top=h+'px'
-    imz.style.left=j+'px'
+//             time_number.innerHTML=counter++;
+//             },1000);
+//         setInterval(()=>{
+//             if(counter<=30){
+//             let h=Math.random()*(a)
+//             let j=Math.random()*(b)
+//             let imz = document.createElement("img")
+//     imz.setAttribute("src","https://freepngimg.com/thumb/anime/12-2-anime-download-png-thumb.png")
+//     imz.setAttribute("onclick","imgclick(this)")
+//     imz.classList.add("autocreatimg")
+//     viewportDiv.appendChild(imz)
+//     imz.style.top=h+'px'
+//     imz.style.left=j+'px'
 
-    imz.onclick=()=>{
-      imz.remove()
-      score_number.innerHTML = count_scor++;
+//     imz.onclick=()=>{
+//       imz.remove()
+//       score_number.innerHTML = count_scor++;
 
-    }
-}else{
-    alert("Time Out....\n"+"Your Score ..."+(count_scor-1))
+//     }
+// }else{
+//     alert("Time Out....\n"+"Your Score ..."+(count_scor-1))
     
-}
+// }
 
-        },1000);
-    }
+//         },1000);
+//     }
 
-    third_img.onclick=()=>{
-        select_img.style.display="none"
-        header.style.display="block"
+//     third_img.onclick=()=>{
+//         select_img.style.display="none"
+//         header.style.display="block"
 
         
-        setInterval(()=>{
+//         setInterval(()=>{
         
-            time_number.innerHTML=counter++;
-            },1000);
-        setInterval(()=>{
-            if(counter<=30){
-            let h=Math.random()*(a)
-            let j=Math.random()*(b)
-            let imz = document.createElement("img")
-    imz.setAttribute("src","https://freepngimg.com/thumb/doraemon/35004-7-doraemon-transparent-image-thumb.png")
-    imz.setAttribute("onclick","imgclick(this)")
-    imz.classList.add("autocreatimg")
-    viewportDiv.appendChild(imz)
-    imz.style.top=h+'px'
-    imz.style.left=j+'px'
+//             time_number.innerHTML=counter++;
+//             },1000);
+//         setInterval(()=>{
+//             if(counter<=30){
+//             let h=Math.random()*(a)
+//             let j=Math.random()*(b)
+//             let imz = document.createElement("img")
+//     imz.setAttribute("src","https://freepngimg.com/thumb/doraemon/35004-7-doraemon-transparent-image-thumb.png")
+//     imz.setAttribute("onclick","imgclick(this)")
+//     imz.classList.add("autocreatimg")
+//     viewportDiv.appendChild(imz)
+//     imz.style.top=h+'px'
+//     imz.style.left=j+'px'
 
-    imz.onclick=()=>{
-      imz.remove()
-      score_number.innerHTML = count_scor++;
+//     imz.onclick=()=>{
+//       imz.remove()
+//       score_number.innerHTML = count_scor++;
 
-    }
-}else{
-    alert("Time Out....\n"+"Your Score ..."+(count_scor-1))
-}
+//     }
+// }else{
+//     alert("Time Out....\n"+"Your Score ..."+(count_scor-1))
+// }
 
-        },1000);
-    }
+//         },1000);
+//     }
 
 
 }
